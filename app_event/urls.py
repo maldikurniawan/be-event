2bbcc18a963d1463ee2caccc092a560ce74c454d
maldikurniawan <akurniawan2102@gmail.com>
2025-07-21 11:05:11 +0700

    init

---
+++ app_event/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
